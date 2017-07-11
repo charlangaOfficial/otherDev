cb3574905d04ff5827fce0da5654a2c102826b8a
Carlos Alvarado <calvarado5@llbean.com>
2017-07-11 06:47:04 -0600

    Doing some cleanup for Trace

---
+++ trace/public/config/config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by crca on 7/11/2017.
+ */
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC65DJtnUVePqZgPPOHsxquNHxX6-aRS-0",
+    authDomain: "trace-43b0b.firebaseapp.com",
+    databaseURL: "https://trace-43b0b.firebaseio.com",
+    projectId: "trace-43b0b",
+    storageBucket: "trace-43b0b.appspot.com",
+    messagingSenderId: "163420796400"
+};
+firebase.initializeApp(config);
